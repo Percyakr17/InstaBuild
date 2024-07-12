d819fb0d0d3820734c2ba33d78cb907659c69fe2
Percyakr17 <samarth17garg@gmail.com>
2024-07-12 10:15:56 +0530

    working on info.html

--- lgsf.php
+++ lgsf.php
@@ -1,22 +1,29 @@
 <?php
-if(isset($_POST['Submit'])) {
-  $name = $_POST['name'];
-  $mail = $_POST['email'];
-    $message = $_POST['message'];
-  $data = $name. "\r" .$mail. "\r" .$message. "\n";
-  
-  $file = fopen("query.txt", "a") or die("Unable to open file!");
-  fwrite($file, $data);
-  fclose($file);
+if (isset($_POST['Submit'])) {
+    // Sanitize user input
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+    
+    // Prepare data to write to file
+    $data = "Name: $name\r\nEmail: $email\r\nMessage: $message\r\n---\r\n";
+    
+    // Open the file in append mode
+    $file = fopen("query.txt", "a") or die("Unable to open file!");
+    fwrite($file, $data);
+    fclose($file);
+    
+    // Display confirmation message
+    echo '<html>
+            <head>
+                <title>Submission Successful</title>
+            </head>
+            <body>
+                <h3 align="center">Thank you! Your query has been submitted successfully. We will contact you soon.</h3>
+                <p align="center"><a href="index.html">Home</a></p>
+            </body>
+          </html>';
+} else {
+    echo 'Error: Please submit the form.';
 }
 ?>
-<html>
-  <head>
-    <title>Info</title>
-  </head>
-  <body>
-
-    <h3 align = center> Thankyou ! Your Query submitted Successfully. <br>
-    <a href="index.html">Home</a></h3>
-  </body>
-</html>
