c0bf6b9173208647920c56b10796e0c07847bbda
Percyakr17 <samarth17garg@gmail.com>
2024-07-12 11:23:07 +0530

    working on info.html

--- lgsf.php
+++ lgsf.php
@@ -1,28 +1,35 @@
 <?php
-if (isset($_POST['Submit'])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['Submit'])) {
     // Sanitize user input
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $message = htmlspecialchars($_POST['message']);
+    $name = htmlspecialchars(trim($_POST['name']));
+    $email = htmlspecialchars(trim($_POST['email']));
+    $message = htmlspecialchars(trim($_POST['message']));
     
     // Prepare data to write to file
     $data = "Name: $name\r\nEmail: $email\r\nMessage: $message\r\n---\r\n";
     
     // Open the file in append mode
-    $file = fopen("query.txt", "a") or die("Unable to open file!");
-    fwrite($file, $data);
-    fclose($file);
-    
-    // Display confirmation message
-    echo '<html>
-            <head>
-                <title>Submission Successful</title>
-            </head>
-            <body>
-                <h3 align="center">Thank you! Your query has been submitted successfully. We will contact you soon.</h3>
-                <p align="center"><a href="index.html">Home</a></p>
-            </body>
-          </html>';
+    $file = fopen("query.txt", "a");
+    if ($file) {
+        fwrite($file, $data);
+        fclose($file);
+        
+        // Display confirmation message
+        echo '<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Submission Successful</title>
+                </head>
+                <body>
+                    <h3 align="center">Thank you! Your query has been submitted successfully. We will contact you soon.</h3>
+                    <p align="center"><a href="index.html">Home</a></p>
+                </body>
+              </html>';
+    } else {
+        echo "Unable to open file!";
+    }
 } else {
     echo 'Error: Please submit the form.';
 }
