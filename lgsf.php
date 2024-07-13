e75fed0d9a440dfb82d8dc04b3dfe76bc79b3611
Percyakr17 <samarth17garg@gmail.com>
2024-07-13 22:33:08 +0530

    updating php files

--- lgsf.php
+++ lgsf.php
@@ -8,23 +8,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['Submit'])) {
     
     $file = fopen("query.txt", "a");
     if ($file) {
-        fwrite($file, $data);
-        fclose($file);
-        
-        echo '<!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>Submission Successful</title>
-                </head>
-                <body>
-                    <h3 align="center">Thank you! Your query has been submitted successfully. We will contact you soon.</h3>
-                    <p align="center"><a href="index.html">Home</a></p>
-                </body>
-              </html>';
+        if (fwrite($file, $data) !== false) {
+            fclose($file);
+            echo '<!DOCTYPE html>
+                    <html lang="en">
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <title>Submission Successful</title>
+                    </head>
+                    <body>
+                        <h3 align="center">Thank you! Your query has been submitted successfully. We will contact you soon.</h3>
+                        <p align="center"><a href="index.html">Home</a></p>
+                    </body>
+                  </html>';
+        } else {
+            fclose($file);
+            echo "Error: Unable to write to file!";
+        }
     } else {
-        echo "Unable to open file!";
+        echo "Error: Unable to open file!";
     }
 } else {
     echo 'Error: Please submit the form.';
