c6a189b80ac8d6f85e4afbb59bea5202b8a943ba
Percyakr17 <samarth17garg@gmail.com>
2024-07-14 11:25:33 +0530

    php not working

--- lgsf.php
+++ lgsf.php
@@ -1,20 +1,13 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get the form data
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
-
-    // Create or open the file
     $file = fopen("query.txt", "a");
-
-    // Write the data to the file
     fwrite($file, "Name: $name\n");
     fwrite($file, "Email: $email\n");
     fwrite($file, "Message: $message\n");
     fwrite($file, "-------------------------\n");
-
-    // Close the file
     fclose($file);
 
     echo "Thank you for your message!";
