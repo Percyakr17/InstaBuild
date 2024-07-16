fb677763cba6eb10a0c2cb284cc2c8b21ef297b7
Percyakr17 <samarth17garg@gmail.com>
2024-07-16 20:09:58 +0530

    php code modify

--- submit.php
+++ submit.php
@@ -1,17 +1,20 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
+    $number = htmlspecialchars($_POST['number']);
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
-    $file = fopen("query.txt", "a");
-    fwrite($file, "Name: $name\n");
-    fwrite($file, "Email: $email\n");
-    fwrite($file, "Email: $Contact\n");
-    fwrite($file, "Message: $message\n");
-    fwrite($file, "-------------------------\n");
-    fclose($file);
-    echo "Thank you for your message!";
+
+    $data = "Name : $name, Number : $number , Email : $email , Message : $message" . PHP_EOL;
+
+    $file = 'data.txt';
+
+    if(file_put_contents($file, $data, FILE_APPEND | LOCK_EX)) {
+        echo "Data successfully saved.";
+    } else {
+        echo "Error saving data.";
+    }
 } else {
-    echo "Please submit the form.";
+    echo "No data received.";
 }
 ?>
